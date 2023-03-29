149cb11b83c56e995231f337fda2fe10ab942450
eli <enathan@seas.upenn.edu>
2023-03-28 21:50:06 -0400

    import useState

--- skill-scout/src/App.js
+++ skill-scout/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import ProfilePage from './ProfilePage';
 import HomePage from './HomePage';
